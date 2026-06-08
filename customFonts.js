3947d0c98c5bb0ff8faef836597b872f7e600b43
garinming <ths03013@naver.com>
2026-06-08 13:59:11 +0900

    Delete customFonts.js

--- customFonts.js
+++
@@ -1,8 +0,0 @@
-// Font files placed in public/fonts/
-// Each entry: { name(표시이름), value(저장키), fontFamily(CSS명), family(CSS font-family값), file(파일명) }
-
-export const CUSTOM_FONTS = [
-  { name: '시네마M',    value: 'sinema',     fontFamily: 'SinemaM',    family: "'SinemaM', sans-serif",    file: 'a시네마M.ttf' },
-  { name: 'KoPub 돋움', value: 'kopubdotum', fontFamily: 'KoPubDotum', family: "'KoPubDotum', sans-serif", file: 'KoPubWorld Dotum Medium.ttf' },
-  { name: '묘은또박체', value: 'griounmyo',  fontFamily: 'GriounMyo',  family: "'GriounMyo', sans-serif",  file: '그리운묘은또박체.ttf' },
-];
